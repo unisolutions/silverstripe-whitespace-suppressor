8b7d154ece386709a6b6f7b1b1f0b99359c7003e
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:58:06 +1300

    Converted to PSR-2

--- code/WhitespaceSuppressorRequestProcessor.php
+++ code/WhitespaceSuppressorRequestProcessor.php
@@ -1,28 +1,30 @@
 <?php
 
-class WhitespaceSuppressorRequestProcessor extends RequestProcessor {
+class WhitespaceSuppressorRequestProcessor extends RequestProcessor
+{
 
-	// false or 'none' - disable processing
-	// 'always'        - enable processing in all environments
-	// 'live-only'     - enable processing only on live environment
-	private static $suppress = 'live-only'; // set to false or 'none' to disable processing
+    // false or 'none' - disable processing
+    // 'always'        - enable processing in all environments
+    // 'live-only'     - enable processing only on live environment
+    private static $suppress = 'live-only'; // set to false or 'none' to disable processing
 
 
-	public function postRequest(SS_HTTPRequest $request, SS_HTTPResponse $response, DataModel $model) {
-		if (preg_match('/text\/html/', $response->getHeader('Content-Type'))) {
-			$response->setBody(
-				$this->SuppressWhitespace($response->getBody())
-			);
-		}
-	}
+    public function postRequest(SS_HTTPRequest $request, SS_HTTPResponse $response, DataModel $model)
+    {
+        if (preg_match('/text\/html/', $response->getHeader('Content-Type'))) {
+            $response->setBody(
+                $this->SuppressWhitespace($response->getBody())
+            );
+        }
+    }
 
-	public function SuppressWhitespace($html) {
-		if ($this->config()->suppress == 'always' || ($this->config()->suppress == 'live-only' && Director::isLive())) {
-			$html = preg_replace("/\s+/", ' ', trim($html));
-			$html = str_replace(array('<!-- -->', ' //<![CDATA[', '//]]> '), '', $html);
-			// TODO: remove all comments?
-		}
-		return $html;
-	}
-
+    public function SuppressWhitespace($html)
+    {
+        if ($this->config()->suppress == 'always' || ($this->config()->suppress == 'live-only' && Director::isLive())) {
+            $html = preg_replace("/\s+/", ' ', trim($html));
+            $html = str_replace(array('<!-- -->', ' //<![CDATA[', '//]]> '), '', $html);
+            // TODO: remove all comments?
+        }
+        return $html;
+    }
 }
